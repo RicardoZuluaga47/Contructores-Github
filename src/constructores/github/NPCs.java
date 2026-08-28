63284eec61880152ceb75ed3512cb584642ad2e9
Ricardo <kleyric@gmail.com>
2026-08-28 15:49:36 -0500

    Creacion de constructores de la ClaseHija NPCs

--- src/constructores/github/NPCs.java
+++ src/constructores/github/NPCs.java
@@ -3,4 +3,19 @@ public class NPCs extends Personajes{
     private String profecion = "";
     private boolean interaccion;
     private int amistad = 0;
+    
+    public NPCs(double vida, int nivel, String nombre, double daño, String profecion, boolean interaccion, int amistad){
+        super(vida, nivel, nombre, daño);
+        this.profecion = profecion;
+        this.interaccion = interaccion;
+        this.amistad = amistad;
+    }
+    
+    public NPCs(String nombre, double vida, String profecion){
+        super(nombre, vida);
+        this.profecion = profecion;
+    }
+    
+    public NPCs(){        
+    }
 }
