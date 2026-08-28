c1cfcb22f9e59e307d7083b6e668319e854b79c8
Ricardo <kleyric@gmail.com>
2026-08-28 15:44:02 -0500

    Creacion de constructores de la ClaseHija Enemigos

--- src/constructores/github/Enemigos.java
+++ src/constructores/github/Enemigos.java
@@ -3,4 +3,19 @@ public class Enemigos extends Personajes{
     private String raza = "";
     private boolean jefe;
     private int recompensa = 0;
+    
+    public Enemigos(double vida, int nivel, String nombre, double daño, String raza, boolean jefe, int recompensa){
+        super(vida, nivel, nombre, daño);
+        this.raza = raza;
+        this.jefe = jefe;
+        this.recompensa = recompensa;
+    }
+    
+    public Enemigos(String nombre, double vida, String raza){
+        super(nombre, vida);
+        this.raza = raza;
+    }
+    
+    public Enemigos(){        
+    }
 }
