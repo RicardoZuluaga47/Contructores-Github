c19c156a95b4522ceb636c3501b098936b29f1f9
Ricardo <kleyric@gmail.com>
2026-08-28 15:09:53 -0500

    Creacion de constructores de la SuperClase Personajes

--- src/constructores/github/Personajes.java
+++ src/constructores/github/Personajes.java
@@ -4,4 +4,20 @@ public class Personajes {
     private int nivel = 0;
     private String nombre = "";
     private double daño = 0;
+    
+    public Personajes(double vida, int nivel, String nombre, double daño){
+        this.daño = daño;
+        this.nivel = nivel;
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+    
+    public Personajes(String nombre, double vida){
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+    
+    public Personajes(){        
+    }
+    
 }
